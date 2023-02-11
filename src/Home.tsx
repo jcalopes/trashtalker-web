93c6ff3d932217412c32ad267579531c98e0146e
jcalopes <lopes.joao.ca@gmail.com>
2023-02-11 18:08:07 +0000

    Established communication with routes service

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,32 +1,20 @@
-import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useEffect, useState } from 'react';
+import { getRoutes } from './api/RoutesService';
+import { Route } from './model/Route';
 
 
 function Home() {
-    const { t, i18n} = useTranslation(['home']);
-    const [counter, useCounter] = useState(0);
+    const [routes, setRoutes] = useState<Route[]>();
+
+    useEffect(()=>{
+        getRoutes().then(routesList => setRoutes(routesList));
+    },[]);
 
     return (
         <div>
-            <div>
-                    <button value= "en" onClick={() => i18n.changeLanguage('en')}>en</button>
-                    <button value= "de" onClick={() => i18n.changeLanguage('de')}>de</button>
-            </div>
-            <div>
-                <h1>{t('title')}</h1>
-                <h3>{t('subtitle')}</h3>
-                <form>
-                    <div>
-                        <span>{counter}/15</span><br/>
-                        <input type="text" 
-                            onChange={(ev) => useCounter(ev.target.value.length)} 
-                            maxLength={15} 
-                            id='fname' 
-                            name='fname'/>
-                        <button type="button">{t('button')}</button>
-                    </div>
-                </form>
-            </div>
+            <>
+            {routes?.map((route,index) => (<span key={`route-${index}`}>{route.name}</span>))}
+            </>
         </div>
     );
 }
